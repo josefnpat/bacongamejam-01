7160373159c1b887d9b7cc3a430bda4f232a0a1d
Seppi <josefnpat@hotmail.com>
2011-11-14 12:37:06 -0500

    Adding tests and documentation. This commit and after is no longer a part of the BaconGameJam. See the file "Flight 2942 - bacongamejam-01.love" for the bacongamejam release.

---
+++ tests/http/main.lua
@@ -0,0 +1,13 @@
+local http = require("socket.http")
+function love.load()
+  local urls = {"http://cupm.net/test","http://google.com","http://localhost"}
+  for i,v in ipairs(urls) do
+    server_data = http.request(v)
+    if server_data then
+      print("[success] "..v)    
+    else
+      print("[failure] "..v)
+    end
+  end
+  love.event.push("q")
+end
